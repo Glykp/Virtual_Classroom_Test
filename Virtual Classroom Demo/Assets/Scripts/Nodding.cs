4ec932ef1790fd30d9ce125e684b2648877c614f
Glykp <31264762+Glykp@users.noreply.github.com>
2020-06-10 19:33:33 +0800

    Added Scenario_5

---
+++ Virtual Classroom Demo/Assets/Scripts/Nodding.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nodding : MonoBehaviour
+{
+    public Animator Anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            Anim.SetBool("isNodding", true);
+        }
+    }
+}
---
+++ Virtual Classroom Demo/Assets/Scripts/ThinkBubble.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThinkBubble : MonoBehaviour
+{
+    public Image Bubble;
+    public Sprite A;
+    public Sprite B;
+    public Sprite Grey;
+    public float RandTime;
+    public float RandChoosingTime;
+    public float Rand;
+    private bool isChosed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bubble = GetComponent<Image>();
+        Bubble.sprite = null;
+        Bubble.CrossFadeAlpha(0, 0.01f, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartChoosing();
+    }
+
+    public void StartChoosing()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (isChosed == false)
+                StartCoroutine(Choosingcoroutine());
+        }
+    }
+
+    IEnumerator Choosingcoroutine()
+    {
+        isChosed = true;
+
+        RandChoosingTime = Random.Range(4.0f, 8.0f);
+        RandTime = Random.Range(2.0f, 6.0f);
+        Rand = Random.Range(0.0f, 1.0f);
+
+        yield return new WaitForSeconds(RandTime);
+        Bubble.sprite = Grey;
+        Bubble.CrossFadeAlpha(.85f, 2.0f, false);
+        yield return new WaitForSeconds(RandChoosingTime);
+        Bubble.CrossFadeAlpha(0.0f, 1.0f, false);
+        yield return new WaitForSeconds(1f);
+        if (Rand > 0.5f)
+            Bubble.sprite = A;
+        else if (Rand <= 0.5f)
+            Bubble.sprite = B;
+        Bubble.CrossFadeAlpha(.85f, 2.0f, false);
+    }
+}
